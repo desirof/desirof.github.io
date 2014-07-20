23f400f202051e0a11771b5bf7fc492f74d5922d
desirof <fdesiro@gmail.com>
2014-07-20 15:22:18 -0700

    .
    .

---
+++ js/prismic-configuration.js
@@ -0,0 +1,60 @@
+define(function() {
+  
+  /** Prismic Configuration **/
+  return {
+
+    apiEndpoint: 'https://lesbonneschoses.prismic.io/api',
+
+    // -- Access token if the Master is not open
+    // accessToken: 'xxxxxx',
+
+    // OAuth
+    clientId: 'UgOwo9_mqQEGd6gl',
+    clientSecret: '3e88a047c8cdc13d0042a7a7e612db9e',
+
+    // -- Links resolution rules
+    linkResolver: function(ctx, doc, isBroken) {
+
+      if(doc.id == ctx.api.bookmarks['about']) {
+        return '/#about' + ctx.maybeRefParam;
+      }
+
+      if(doc.id == ctx.api.bookmarks['jobs']) {
+        return '/#jobs' + ctx.maybeRefParam;
+      }
+
+      if(doc.id == ctx.api.bookmarks['stores']) {
+        return '/#stores' + ctx.maybeRefParam;
+      }
+
+      if(doc.type == 'store' && !isBroken) {
+        return '/#stores' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
+      }
+
+      if(doc.type == 'product' && !isBroken) {
+        return '/#products' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
+      }
+
+      if(doc.type == 'selection' && !isBroken) {
+        return '/#selections' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
+      }
+
+      if(doc.type == 'job-offer' && !isBroken) {
+        return '/#jobs' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
+      }
+
+      if(doc.type == 'blog-post' && !isBroken) {
+        return '/blog.html#posts' + ctx.maybeRefParam + '/' + doc.id + '/' + doc.slug;
+      }
+
+      return '/#not-found';
+    },
+
+    // -- To customize: what to do when an error happens on the prismic.io side
+    onPrismicError: function(err) {
+      alert("An error happened on the server side: "+(err ? err.message : ''));
+    }
+
+  };
+
+});
